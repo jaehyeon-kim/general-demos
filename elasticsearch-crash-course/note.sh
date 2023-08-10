8437b5b872f27cdac8549d82487c5cba37bb82c2
Jaehyeon Kim <dottami@gmail.com>
2023-08-11 09:21:43 +1000

    update part 2

--- elasticsearch-crash-course/note.sh
+++ elasticsearch-crash-course/note.sh
@@ -1,4 +1,5 @@
 ###### PART 1
+# https://www.youtube.com/playlist?list=PL_mJOmq4zsHZYAyK606y7wjQtC0aoE6Es
 # https://github.com/LisaHJung/Beginners-Crash-Course-to-Elastic-Stack-Series-Table-of-Contents
 
 curl localhost:9200
@@ -35,3 +36,108 @@ curl -X POST localhost:9200/favourite_candy/_update/0 \
 
 ## delete
 curl -X DELETE localhost:9200/favourite_candy/_doc/0
+
+###### PART 2
+
+# Precision = True positives / (True positives <relevant and retrieved> + False positives <irrelevant but retrieved>)
+#  => what portition of the retrieved data is actually relevant to the search query 
+# Recall = True positives / (True positives + False negatives <relevant but not retrieved>)
+#  => what portiton of relevant data is being returned as search results?
+
+# Precision focuses on relevance (quality) while recall on quantity, they are inversely related
+
+# search results are ranked by score
+
+# score? 
+#   term frequency (TF) - how many times each search term appears in a document
+#   inverse document frequency (IDF) - if some terms appear in many documents, they are consiered as unimportant
+
+# dataset - https://www.kaggle.com/datasets/rmisra/news-category-dataset
+
+```
+GET news_headlines/_search
+
+GET news_headlines/_search
+{
+  "track_total_hits": true
+}
+
+GET news_headlines/_search
+{
+  "query": {
+    "range": {
+      "date": {
+        "gte": "2015-06-20",
+        "lte": "2015-09-22"
+      }
+    }
+  }
+}
+
+GET news_headlines/_search
+{
+  "aggs": {
+    "by_category": {
+      "terms": {
+        "field": "category",
+        "size": 100
+      }
+    }
+  }
+}
+
+GET news_headlines/_search
+{
+  "query": {
+    "match": {
+      "category": "ENTERTAINMENT"
+    }
+  },
+  "aggs": {
+    "popular_in_entertainment": {
+      "significant_text": {
+        "field": "headline"
+      }
+    }
+  }
+}
+
+GET news_headlines/_search
+{
+  "query": {
+    "match": {
+      "headline": {
+        "query": "Khloe Kardashian Kendall Jenner"
+      }
+    }
+  }
+}
+
+GET news_headlines/_search
+{
+  "query": {
+    "match": {
+      "headline": {
+        "query": "Khloe Kardashian Kendall Jenner",
+        "operator": "and"
+      }
+    }
+  }
+}
+
+GET news_headlines/_search
+{
+  "query": {
+    "match": {
+      "headline": {
+        "query": "Khloe Kardashian Kendall Jenner",
+        "minimum_should_match": 3
+      }
+    }
+  }
+}
+
+DELETE news_headlines
+
+GET news_headlines/_search
+```
