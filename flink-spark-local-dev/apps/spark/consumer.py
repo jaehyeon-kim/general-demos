d9df53a0e1ad9217275da2c8b440251a6a51683b
Jaehyeon Kim <dottami@gmail.com>
2023-12-01 13:56:17 +1100

    update comment

--- flink-spark-local-dev/apps/spark/consumer.py
+++ flink-spark-local-dev/apps/spark/consumer.py
@@ -2,8 +2,16 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
     """
-    docker exec spark /usr/lib/spark/bin/spark-submit \
-        --master local[*] --deploy-mode client /home/hadoop/project/apps/spark/consumer.py
+    ## spark submit
+    docker exec spark spark-submit \
+        --master local[*] \
+        --deploy-mode client \
+        /home/hadoop/project/apps/spark/consumer.py
+    
+    ## pyspark
+    docker exec -it spark pyspark \
+        --master local[*] \
+        --deploy-mode client 
     """
     spark = SparkSession.builder.appName("Consume Orders").getOrCreate()
     spark.sparkContext.setLogLevel("FATAL")
