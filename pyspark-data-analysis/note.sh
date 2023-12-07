947dd9c224397cd9008d95d41bea7151c3656cee
Jaehyeon Kim <dottami@gmail.com>
2023-12-07 12:30:18 +1100

    cherry pick

---
+++ pyspark-data-analysis/note.sh
@@ -0,0 +1,15 @@
+# PySpark Overview
+# https://spark.apache.org/docs/latest/api/python/index.html
+
+export SCRIPT_PATH=/home/hadoop/project/apps/spark/code/ch03.py
+
+## spark submit
+docker exec spark spark-submit \
+  --master local[*] --deploy-mode client $SCRIPT_PATH
+
+## pyspark
+docker exec -it spark pyspark --master local[*] --deploy-mode client 
+
+export PYTHONPATH="${SPARK_HOME}/python:${PYTHONPATH}" \
+  && export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.10.9.7-src.zip:${PYTHONPATH}" \
+  && export PYSPARK_PYTHONPATH_SET=1
