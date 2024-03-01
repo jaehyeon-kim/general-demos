57853ed120801af477fbf4a488d4c93686c5fd03
Jaehyeon Kim <dottami@gmail.com>
2024-03-01 16:05:01 +1100

    add part 2 examples

---
+++ beam-dev-env/part3/user_traffic.py
@@ -0,0 +1,99 @@
+import os
+import datetime
+import argparse
+import json
+import logging
+import typing
+
+import apache_beam as beam
+from apache_beam.transforms.combiners import CountCombineFn
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import StandardOptions
+
+
+class EventLog(typing.NamedTuple):
+    ip: str
+    id: str
+    lat: float
+    lng: float
+    user_agent: str
+    age_bracket: str
+    opted_into_marketing: bool
+    http_request: str
+    http_response: int
+    file_size_bytes: int
+    event_datetime: str
+    event_ts: int
+
+
+class PerUserAggregate(typing.NamedTuple):
+    id: str
+    page_views: int
+    total_bytes: int
+    max_bytes: int
+    min_bytes: int
+
+
+beam.coders.registry.register_coder(EventLog, beam.coders.RowCoder)
+beam.coders.registry.register_coder(PerUserAggregate, beam.coders.RowCoder)
+
+
+def parse_json(element: str):
+    row = json.loads(element)
+    # lat/lng sometimes empty string
+    if not row["lat"] or not row["lng"]:
+        row = {**row, **{"lat": -1, "lng": -1}}
+    return EventLog(**row)
+
+
+def run():
+    parser = argparse.ArgumentParser(description="Process website visit event")
+    parser.add_argument(
+        "--inputs",
+        default="inputs",
+        help="Specify folder name that event records are saved",
+    )
+    parser.add_argument(
+        "--runner", default="DirectRunner", help="Specify Apache Beam Runner"
+    )
+    opts = parser.parse_args()
+    PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+    options = PipelineOptions()
+    options.view_as(StandardOptions).runner = opts.runner
+
+    p = beam.Pipeline(options=options)
+    (
+        p
+        | "Read from files"
+        >> beam.io.ReadFromText(
+            file_pattern=os.path.join(PARENT_DIR, opts.inputs, "*.out")
+        )
+        | "Parse elements" >> beam.Map(parse_json).with_output_types(EventLog)
+        | "Aggregate by user"
+        >> beam.GroupBy("id")
+        .aggregate_field("id", CountCombineFn(), "page_views")
+        .aggregate_field("file_size_bytes", sum, "total_bytes")
+        .aggregate_field("file_size_bytes", max, "max_bytes")
+        .aggregate_field("file_size_bytes", min, "min_bytes")
+        .with_output_types(PerUserAggregate)
+        | "To Dict" >> beam.Map(lambda e: e._asdict())
+        | "Write to file"
+        >> beam.io.WriteToText(
+            file_path_prefix=os.path.join(
+                PARENT_DIR,
+                "outputs",
+                f"{int(datetime.datetime.now().timestamp() * 1000)}",
+            ),
+            file_name_suffix=".out",
+        )
+    )
+
+    logging.getLogger().setLevel(logging.INFO)
+    logging.info("Building pipeline ...")
+
+    p.run()
+
+
+if __name__ == "__main__":
+    run()
