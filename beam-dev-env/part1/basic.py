d007e472e4e8304d61b6546dc193dc19bc68a5c1
Jaehyeon Kim <dottami@gmail.com>
2024-03-01 14:36:40 +1100

    re-arrange part 1

--- beam-dev-env/part1/basic.py
+++ beam-dev-env/part1/basic.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import json
 import logging
@@ -11,12 +12,15 @@ from apache_beam.options.pipeline_options import StandardOptions
 def run():
     parser = argparse.ArgumentParser(description="Process website visit event")
     parser.add_argument(
-        "--path", default="inputs", help="Specify path that event records are saved"
+        "--inputs",
+        default="inputs",
+        help="Specify folder name that event records are saved",
     )
     parser.add_argument(
         "--runner", default="DirectRunner", help="Specify Apache Beam Runner"
     )
     opts = parser.parse_args()
+    PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
     options = PipelineOptions()
     options.view_as(StandardOptions).runner = opts.runner
@@ -24,7 +28,10 @@ def run():
     p = beam.Pipeline(options=options)
     (
         p
-        | "Read from files" >> beam.io.ReadFromText(file_pattern=f"{opts.path}/*.out")
+        | "Read from files"
+        >> beam.io.ReadFromText(
+            file_pattern=os.path.join(PARENT_DIR, opts.inputs, "*.out")
+        )
         | "Parse Json" >> beam.Map(lambda line: json.loads(line))
         | "Filter status" >> beam.Filter(lambda d: d["opted_into_marketing"] is True)
         | "Select columns"
@@ -37,7 +44,10 @@ def run():
         )
         | "Write to file"
         >> beam.io.WriteToText(
-            file_path_prefix=f"outputs/{str(uuid.uuid4())}-", file_name_suffix=".out"
+            file_path_prefix=os.path.join(
+                PARENT_DIR, "outputs", f"{str(uuid.uuid4())}-"
+            ),
+            file_name_suffix=".out",
         )
     )
 
