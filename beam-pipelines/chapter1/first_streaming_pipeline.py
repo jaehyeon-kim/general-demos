16c39843c4ead4a167b7960b1a2dc310bb30848c
Jaehyeon Kim <dottami@gmail.com>
2024-03-18 05:10:57 +1100

    update chapter 1

--- beam-pipelines/chapter1/first_streaming_pipeline.py
+++ beam-pipelines/chapter1/first_streaming_pipeline.py
@@ -1,9 +1,15 @@
 import os
+import datetime
 import argparse
 import logging
 import re
 
 import apache_beam as beam
+from apache_beam.coders import coders
+from apache_beam.transforms import window
+from apache_beam.transforms.trigger import AfterWatermark, AccumulationMode
+from apache_beam.testing.test_stream import TestStream
+from apache_beam.transforms.window import TimestampedValue
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import StandardOptions
 
@@ -33,12 +39,27 @@ def run():
     options = PipelineOptions()
     options.view_as(StandardOptions).runner = opts.runner
 
+    lines = read_file("lorem.txt", os.path.join(PARENT_DIR, "inputs"))
+    now = int(datetime.datetime.now().timestamp() * 1000)
+    test_stream = (
+        TestStream(coder=coders.StrUtf8Coder())
+        .with_output_types(str)
+        .add_elements(
+            [TimestampedValue(lines[i], now + 1000) for i in range(len(lines))]
+        )
+        .advance_watermark_to_infinity()
+    )
+
     p = beam.Pipeline(options=options)
     (
         p
-        | "Read file"
-        >> beam.Create([read_file("lorem.txt", os.path.join(PARENT_DIR, "inputs"))])
-        | "to string" >> beam.ToString.Iterables()
+        | "Read stream" >> test_stream
+        | "Windowing"
+        >> beam.WindowInto(
+            window.GlobalWindows(),
+            trigger=AfterWatermark(),
+            accumulation_mode=AccumulationMode.DISCARDING,
+        )
         | "Extract words" >> beam.FlatMap(tokenize)
         | "Count per word" >> beam.combiners.Count.PerElement()
         | beam.Map(print)
--- beam-pipelines/chapter1/missing_window_pipeline.py
+++ beam-pipelines/chapter1/missing_window_pipeline.py
@@ -1,9 +1,13 @@
 import os
+import datetime
 import argparse
 import logging
 import re
 
 import apache_beam as beam
+from apache_beam.coders import coders
+from apache_beam.testing.test_stream import TestStream
+from apache_beam.transforms.window import TimestampedValue
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import StandardOptions
 
@@ -33,12 +37,20 @@ def run():
     options = PipelineOptions()
     options.view_as(StandardOptions).runner = opts.runner
 
+    lines = read_file("lorem.txt", os.path.join(PARENT_DIR, "inputs"))
+    now = int(datetime.datetime.now().timestamp() * 1000)
+    test_stream = (
+        TestStream(coder=coders.StrUtf8Coder())
+        .add_elements(
+            [TimestampedValue(lines[i], now + 1000) for i in range(len(lines))]
+        )
+        .advance_watermark_to_infinity()
+    )
+
     p = beam.Pipeline(options=options)
     (
         p
-        | "Read file"
-        >> beam.Create([read_file("lorem.txt", os.path.join(PARENT_DIR, "inputs"))])
-        | "to string" >> beam.ToString.Iterables()
+        | "Read stream" >> test_stream
         | "Extract words" >> beam.FlatMap(tokenize)
         | "Count per word" >> beam.combiners.Count.PerElement()
         | beam.Map(print)
